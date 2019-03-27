dd4ea0d13a38fde90b8a56d86c48c0758b3efd8d
rupak <rupak4@yandex.ru>
2019-03-27 10:04:06 +0300

    3. новый шаблон

--- resources/views/objects.blade.php
+++ resources/views/objects.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    1
                 </div>
             </div>
         </div>
