0885d0b5adbdb37eb8d537c761e004ca1c3ba1f7
rupak <rupak4@yandex.ru>
2019-03-28 11:38:04 +0300

    initial

--- Makefile
+++ Makefile
@@ -1,13 +1,10 @@
-test:
-		composer run-script phpunit
-serve:
-	    php artisan serve
 install:
 		composer install
 env:
 		cp .env.example .env
 key:
 		php artisan key:generate
-        php artisan config:cache
 lint:
 		composer run-script phpcs -- --standard=PSR12  app routes tests
+test:
+		composer run-script phpunit
--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Recca0120\LaravelTracy\LaravelTracyServiceProvider::class,
         /*
          * Package Service Providers...
          */
