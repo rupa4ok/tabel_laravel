2d1fcc660092c8293a47da1aa4942468b9c55fd1
rupak <rupak4@yandex.ru>
2019-03-28 11:42:52 +0300

    initial

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-03-28 08:17:37.
+    * Generated on 2019-03-28 08:40:47.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
