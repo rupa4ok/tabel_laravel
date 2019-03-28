1befd6911f46acb5fb9d4294237fb4a8d3e18875
rupak <rupak4@yandex.ru>
2019-03-28 14:34:51 +0300

    5. Новый шаблон

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.28 on 2019-03-28 08:45:26.
+ * Generated for Laravel 5.7.28 on 2019-03-28 11:31:18.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
